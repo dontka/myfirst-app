8de06fc1ac6af8de71ee0182afeb172cabd27463
Alphonse243 <alphonsekatumbescience@gmail.com>
2026-02-07 13:30:07 +0200

    UP date model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,12 @@ class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+    /**
+     * Les attributs qui peuvent être remplis en masse.
+     * Cela permet d'utiliser Post::create(['title' => '...', 'content' => '...'])
+     */
+    protected $fillable = [
+        'title',
+        'content',
+    ];
 }
